58d880211074cf9124773de97f0a949be47fa880
holagus <agustin.catellani@gmail.com>
2025-01-19 20:30:09 -0300

    fix: use atomic read instruction instead of atomic read/update

--- src/sbi.zig
+++ src/sbi.zig
@@ -52,9 +52,7 @@ fn syscall_1(arg0: c_long, fid: c_long, eid: c_long) SbiRet {
 /// Read the value of the CSR named `regname`
 pub fn read_csr(comptime regname: []const u8) usize {
     var ret: u32 = undefined;
-    // csrrc csr, rd, rs
-    // ret <- csr, csr <- csr & ~rs
-    asm volatile ("csrrc a0, " ++ regname ++ ", x0"
+    asm volatile ("csrr a0, " ++ regname
         : [ret] "={a0}" (ret),
     );
     return ret;
