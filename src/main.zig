a5c853f38326db885f2d62498904e9932ea44244
holagus <agustin.catellani@gmail.com>
2025-03-19 20:20:14 -0300

    feat: do some allocations to test if the allocator works

--- src/main.zig
+++ src/main.zig
@@ -8,8 +8,15 @@ pub fn os_main() void {
     // Should panic
     // @panic("Whoops!");
 
+    var arena = std.heap.ArenaAllocator.init(os.OsAllocator());
+    var arr = std.ArrayList(usize).init(arena.allocator());
+
+    for (0..256) |x| {
+        arr.append(x) catch @panic("Memory Error");
+    }
+
     while (true) {
-        // os.println("Current time: {}", .{os.time()});
-        // asm volatile ("wfi");
+        os.println("Current time: {}", .{os.time()});
+        arr.append(0) catch @panic("Memory Error");
     }
 }
